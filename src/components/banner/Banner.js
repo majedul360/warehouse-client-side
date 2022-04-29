b19e24143104f2e3ac1cad33049a68b50100e998
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-04-29 15:16:51 +0600

    banner section done

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -9,6 +9,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "./Banner.css";
+import Btn from "../btn/Btn";
 const Banner = () => {
   return (
     <div className="banner">
@@ -20,15 +21,33 @@ const Banner = () => {
         slidesPerView={1}
         navigation
       >
-        <SwiperSlide className="slide slide-1"></SwiperSlide>
-        <SwiperSlide className="slide slide-2"></SwiperSlide>
+        <SwiperSlide className="slide slide-1">
+          <div className="content">
+            <p style={{ fontSize: "2.5rem" }}>
+              natural health care ingredients
+            </p>
+            <h1>grocery shopping</h1>
+            <p style={{ fontSize: "3rem" }}>save upto 30% off</p>
+            <Btn className="btn">shop now</Btn>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="slide slide-2">
+          <div className="content">
+            <p style={{ fontSize: "3.5rem" }}>groca</p>
+            <h1>vegetable 100% </h1>
+            <h1>organic</h1>
+            <p style={{ fontSize: "3rem" }}>save upto 30% off</p>
+            <Btn className="btn">shop now</Btn>
+          </div>
+        </SwiperSlide>
         <SwiperSlide className="slide slide-3">
           <div className="content">
             <img src="images/slider-icon.png" alt="" />
-            <p>100% healthy & affordable</p>
+            <p style={{ fontSize: "2.5rem" }}>100% healthy & affordable</p>
             <h1>organic</h1>
             <h1>vegetables</h1>
-            <p>small changes big differemce</p>
+            <p style={{ fontSize: "3rem" }}>small changes big differemce</p>
+            <Btn>shop now</Btn>
           </div>
         </SwiperSlide>
       </Swiper>
---
+++ src/components/btn/Btn.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Btn.css";
+const Btn = ({ children }) => {
+  return <div className="btn">{children}</div>;
+};
+
+export default Btn;
