1a57e318c46bd2eeacea8901f2c54c91d48050db
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-04-30 16:50:22 +0600

    add quantity functionality done

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -5,9 +5,11 @@ import "./Details.css";
 const Details = () => {
   const { id } = useParams();
   const [iteam, setIteam] = useState({});
-  let [delivar, setDelivar] = useState(0);
+  const [delivar, setDelivar] = useState(0);
+  const [number, setNumber] = useState(0);
   const { _id, name, img, price, supplierName, desc, quantity } = iteam;
-
+  const finalQuantity =
+    parseInt(quantity) - parseInt(delivar) + parseInt(number);
   useEffect(() => {
     axios
       .get(`http://localhost:5000/${id}`)
@@ -17,7 +19,7 @@ const Details = () => {
       .catch((error) => console.log(error));
   }, [id]);
 
-  const delivarQuantity = (id) => {
+  const delivarQuantity = () => {
     const quan = delivar + 1;
     setDelivar(quan);
     const newQuantity = parseInt(quantity) - quan;
@@ -26,15 +28,21 @@ const Details = () => {
     };
     axios
       .put(`http://localhost:5000/update/${_id}`, iteamObj)
-      .then((data) => console.log(data))
+      .then((data) => {})
       .catch((error) => console.log(error));
   };
 
-  //   const formHandler = (e) => {
-  //     e.preventDefault();
-  //     const number = e.target.number.value;
-  //     console.log(number);
-  //  };
+  const formHandler = (e) => {
+    e.preventDefault();
+    const number = e.target.number.value;
+    setNumber(number);
+    const newQuantity = parseInt(quantity) + parseInt(number);
+    const iteamObj = { quantity: newQuantity };
+    axios
+      .put(`http://localhost:5000/update/${_id}`, iteamObj)
+      .then((data) => e.target.reset())
+      .catch((error) => console.log(error));
+  };
   return (
     <div className="details">
       <div className="box-container">
@@ -43,7 +51,7 @@ const Details = () => {
           <div className="content">
             <h3>{name}</h3>
             <h4>{price}</h4>
-            <h5>available: {parseInt(quantity) - delivar}</h5>
+            <h5>available: {isNaN(finalQuantity) ? "" : finalQuantity}</h5>
             <h6>supplier: {supplierName}</h6>
           </div>
         </div>
@@ -55,7 +63,7 @@ const Details = () => {
         </div>
       </div>
 
-      <form action="">
+      <form onSubmit={formHandler} action="">
         <input
           className="input"
           name="number"
