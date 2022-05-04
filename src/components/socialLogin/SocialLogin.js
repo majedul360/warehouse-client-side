bd53e71ed44f9ce81db73f012ea5fbc31477c820
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-05-04 12:32:18 +0600

    social login require auth replace done

--- src/components/socialLogin/SocialLogin.js
+++ src/components/socialLogin/SocialLogin.js
@@ -2,9 +2,17 @@ import React from "react";
 import "./SocialLogin.css";
 import auth from "../firebase/Firebase.int";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Sociallogin = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
   return (
     <div className="social-login">
       <div className="social-login-btn" onClick={() => signInWithGoogle()}>
