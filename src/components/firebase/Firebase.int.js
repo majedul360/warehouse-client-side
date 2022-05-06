ae7ad33edd99146682f0c9c8ba19139d0a6e8fd5
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-05-06 20:17:26 +0600

    environment variable implement done

--- src/components/firebase/Firebase.int.js
+++ src/components/firebase/Firebase.int.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCv5cRdoaenWSoe5iG_Dd5NJ1BvazHJwgs",
-  authDomain: "groca-2cea6.firebaseapp.com",
-  projectId: "groca-2cea6",
-  storageBucket: "groca-2cea6.appspot.com",
-  messagingSenderId: "142617441415",
-  appId: "1:142617441415:web:436eaf6c3fbc6ae88ab422",
+  apiKey: process.env.REACT_APP_ApiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
